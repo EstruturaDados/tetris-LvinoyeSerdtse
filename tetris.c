1f4edff03ab31cf4687fade9477e795b82ed115b
LvinoyeSerdtse <srennan156@gmail.com>
2026-04-16 12:56:51 -0300

    RENNAN SILVA DA GRAÇA - fiz o segundo desafio, incluindo pilha.

--- tetris.c
+++ tetris.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX 5
+#define MAX_PILHA 3
+
 // estrutura 'Peca'
 typedef struct {
 char tipo;
@@ -16,29 +19,52 @@ int fim;
 int total;
 }Fila;
 
-// prototipos de funcao
-void Menu(Fila *f);
+typedef struct {
+Peca reservas[MAX_PILHA];
+int topo;
+}Pilha;
+
+
+// 1 - PROTOTIPOS DE FUNCAO
+void mainMenu(Fila *f, Pilha *r);
+
+// 1.1 - funcoes de fila
 void inicializarFila(Fila *f);
 void inserir(Fila *f, Peca pp);
 void remover(Fila *f, Peca *p);
 void exibirFila(Fila *f);
 void gerarPeca(Peca *p, int idGerado);
-void mainMenu(Fila *f);
+
+// 1.2 - funcoes de pilha
+int pilhaCheia(Pilha *r);
+int pilhaVazia(Pilha *r);
+void inicializarPilha(Pilha *r);
+void exibirPilha(Pilha *r);
+void peek(Pilha *r, Peca *escolhida);
+void pop(Pilha *r, Peca *removida);
+void push(Pilha *r, Peca inserida);
+// 1.3 - funcoes "in-game" da pilha
+void reservarPeca(Fila *fila, Pilha *pilha);
+Peca usarReserva(Pilha *p);
+
 
 // funcao principal, onde o programa de fato roda
 int main(){
     srand(time(NULL));
 
     Fila minhaFila;
-    inicializarFila(&minhaFila);
+    Pilha duracell;
 
-    mainMenu(&minhaFila);
+    inicializarFila(&minhaFila);
+    inicializarPilha(&duracell);
+
+    mainMenu(&minhaFila, &duracell);
 
 return 0;
 }
 
 
-void mainMenu(Fila *f) {
+void mainMenu(Fila *f, Pilha *r) {
     printf("TETRIS (inserir aquela musiquinha legal)\n");
     printf("=============================\n");
     int opcao;
@@ -51,7 +77,9 @@ void mainMenu(Fila *f) {
     printf("Opções:\n");
     printf("[1] - JOGAR PEÇA\n");
     printf("[2] - INSERIR NOVA PEÇA\n");
-    printf("[3] - PECAS DISPONIVEIS\n");
+    printf("[3] - RESERVAR PEÇA\n");
+    printf("[4] - USAR PEÇA RESERVA\n");
+    printf("[5] - PECAS DISPONIVEIS\n");
     printf("[0] - SAIR DO JOGO\n\n\n");
     printf("OPCAO: ");
     scanf("%d", &opcao);
@@ -105,9 +133,104 @@ void mainMenu(Fila *f) {
         break;
 
         case 3:
+            printf("\nReservando peça...");
+            if(pilhaCheia(r)){
+                printf("\n[!] - Pilha cheia!");
+                break;
+            }
+
+            do {
+            // validando escolha do jogador
+            char escolha;
+            exibirFila(f);
+            exibirPilha(r);
+
+            printf("Reservar a primeira peça? (S/N)\n");
+            scanf(" %c", &escolha);
+
+            // se sim, vamos jogar a peca
+            if (escolha == 'S' || escolha == 's') {
+                printf("\nReservando a peça...\n");
+                reservarPeca(f, r);
+                exibirFila(f);
+                printf("\n");
+                exibirPilha(r);
+                break;
+            }
+
+            // se nao, cancelar a operacao que o usuario cogitou fazer quando selecionou a opcao [1]
+            else if (escolha == 'N' || escolha == 'n') {
+                printf("Ok! A peça nao sera reservada.\n");
+                break;
+            }
+
+            // se n digitou N(n) ou S(s), vai repetir a pergunta dps de dizer que a escolha eh invalida 
+            else { 
+                printf("[!] - Escolha invalida\n");
+            }
+
+        } while (1);
+        break;
+    
+    
+        case 4:
+            printf("\nUsando a reserva de peças...");
+            if (pilhaVazia(r)){
+                printf("\n[!] - Pilha vazia!");
+                break;
+            }
+
+            do {
+            // validando escolha do jogador
+            char escolha;
+            Peca pecaDaVez;
+            peek(r, &pecaDaVez);
+
+            exibirFila(f);
+            exibirPilha(r);
+            
+
+            printf("\nA peça jogada será: %c [ID: %d]", pecaDaVez.tipo, pecaDaVez.id);
+
+            printf("\nDeseja usar a peça reserva? (S/N)\n");
+            scanf(" %c", &escolha);
+
+            // se sim, vamos jogar a peca
+            if (escolha == 'S' || escolha == 's') {
+                printf("\nJogando a peça da reserva...");
+                Peca removida;
+                pop(r, &removida);
+
+                exibirFila(f);
+                printf("\n");
+                exibirPilha(r);
+                break;
+            }
+
+            // se nao, cancelar a operacao que o usuario cogitou fazer quando selecionou a opcao [1]
+            else if (escolha == 'N' || escolha == 'n') {
+                printf("\nOk! A peça nao sera reservada.");
+                break;
+            }
+
+            // se n digitou N(n) ou S(s), vai repetir a pergunta dps de dizer que a escolha eh invalida 
+            else { 
+                printf("\n[!] - Escolha invalida\n");
+            }
+
+        } while (1);
+        break;
+
+
+
+            
+
+        case 5:
         // literalmente isso mesmo. exibe as pecas que o usuario tem a disposicao
-        printf("Peças atuais: \n");
+        printf("\nPeças atuais: ");
         exibirFila(f);
+        exibirPilha(r);
+        break;
     } 
 } while (opcao != 0);
 // enquanto o usuario n apertar pra quitar, o menu estara em loop
@@ -124,7 +247,7 @@ void inicializarFila(Fila *f){
 
 void inserir(Fila *f, Peca pp){
     if(f->total == MAX) {
-        printf("[!] - Fila cheia\n\n");
+        printf("\n[!] - Fila cheia\n");
         return;
     }
     f->conjunto[f->fim] = pp;
@@ -134,7 +257,7 @@ void inserir(Fila *f, Peca pp){
 
 void remover(Fila *f, Peca *p){
     if(f->total == 0){
-        printf("[!] - Fila vazia\n\n");
+        printf("\n[!] - Fila vazia\n");
         return;
     }
     *p = f->conjunto[f->inicio];
@@ -144,15 +267,19 @@ void remover(Fila *f, Peca *p){
 }
 
 void exibirFila(Fila *f){
+
+    printf("\nExibindo fila...\n");
+
     if (f->total == 0){
-    printf("[!] - Fila vazia\n\n");
+    printf("\n[!] - Fila vazia\n");
     return;
     }
 
-    printf("Fila:");
+    printf("\nFila:");
     for (int i = 0, idx = f->inicio; i < f->total; i++, idx = (idx + 1) % MAX) {
-    printf("\nPEÇA: %c (ID: %d)\n\n", f->conjunto[idx].tipo, f->conjunto[idx].id);        
+    printf("\nPEÇA: %c (ID: %d)", f->conjunto[idx].tipo, f->conjunto[idx].id);        
     }
+    printf("\n==============");
 }
 
 void gerarPeca(Peca* p, int idGerado) {
@@ -160,3 +287,80 @@ void gerarPeca(Peca* p, int idGerado) {
     p->tipo = tipos[rand() % 4];
     p->id = idGerado;
 }
+
+void reservarPeca(Fila *fila, Pilha *pilha){
+    if (fila->total > 0 && !pilhaCheia(pilha) ) {
+        Peca removidaDaFila;
+        remover(fila, &removidaDaFila);
+        push(pilha, removidaDaFila);
+    }
+    else {
+        printf("\n[!] - Operacao Impossivel!\n");
+    return;
+    }
+}
+
+Peca usarReserva(Pilha *p) {
+        Peca pecaReserva;
+        pop(p, &pecaReserva);
+        return pecaReserva;
+}
+
+
+// implementacao da pilha
+void inicializarPilha(Pilha *p){
+    p->topo = -1;
+}
+
+int pilhaCheia(Pilha *p){
+    return p->topo == MAX_PILHA - 1;
+}
+
+int pilhaVazia(Pilha *p){
+    return p->topo == -1;
+}
+
+void pop(Pilha *p, Peca *removida){
+    if (pilhaVazia(p)) {
+        printf("\n[!] - A pilha está vazia.\n");
+        return;
+    }
+    
+    *removida = p->reservas[p->topo];
+    p->topo--;
+}
+
+void push(Pilha *p, Peca inserida){
+    if (pilhaCheia(p)) {
+        printf("[!] - A pilha está cheia.\n");
+        return;
+    }
+
+    p->topo++;
+    p->reservas[p->topo] = inserida;
+}
+
+void peek(Pilha *p, Peca *escolhida){
+    if(pilhaVazia(p)) {
+        printf("[!] - Pilha vazia!\n");
+        return;
+    }
+    
+    *escolhida = p->reservas[p->topo];
+}
+
+void exibirPilha(Pilha *p){
+
+    printf("\nExibindo reserva...\n");
+    if(pilhaVazia(p)){
+        printf("[!] - Pilha vazia!\n");
+        return;
+    }
+    printf("\nReserva:");
+
+    for (int i = p->topo; i>=0; i--){
+        printf("\nPEÇA: %c (ID: %d)", p->reservas[i].tipo, p->reservas[i].id);        
+    }
+    printf("\n==============");
+}
+
